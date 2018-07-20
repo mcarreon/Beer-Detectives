6a43c93bf42d838139dc633f9f2c6184b4a15d47
hayk ferayan <hayk715@gmail.com>
2018-07-19 20:05:28 -0700

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,36 +38,48 @@ $(document).ready(function () {
 });
 
 
-    var queryURL = "https://api.punkapi.com/v2/beers/random";
-    var call1 = $.ajax({
-      url: queryURL,
-      method: "GET"
-    });
-    var call2 = $.ajax({
-      url: queryURL,
-      method: "GET"
-    });
-    var call3 = $.ajax({
-      url: queryURL,
-      method: "GET"
-    });
+var queryURL = "https://api.punkapi.com/v2/beers/random";
+var call1 = $.ajax({
+  url: queryURL,
+  method: "GET"
+});
+var call2 = $.ajax({
+  url: queryURL,
+  method: "GET"
+});
+var call3 = $.ajax({
+  url: queryURL,
+  method: "GET"
+});
+
+$.when(call1, call2, call3)
+.then(function(response1, response2, response3) {
+    // debugger;
+    var beer = response1[0][0];
+    var beerName = $("<p>").text(beer.name);
+    var beerType = $("<p>").text(beer.tagline);
+    var beerImage = $("<img>").attr("src", beer.image_url);
+    var suggestionWrapper = $('<a class="suggestion">');
+    suggestionWrapper.append(beerImage);
+
+    var beer = response2[0][0];
+    var beerName = $("<p>").text(beer.name);
+    var beerType = $("<p>").text(beer.tagline);
+    var beerImage = $("<img>").attr("src", beer.image_url);
+    var suggestionWrapper2 = $('<a class="suggestion">');
+    suggestionWrapper2.append(beerImage);
+
+    var beer = response3[0][0];
+    var beerName = $("<p>").text(beer.name);
+    var beerType = $("<p>").text(beer.tagline);
+    var beerImage = $("<img>").attr("src", beer.image_url);
+    var suggestionWrapper3 = $('<a class="suggestion">');
+    suggestionWrapper3.append(beerImage);
+
+    $("#suggestions-div").empty().append(suggestionWrapper).append(suggestionWrapper2).append(suggestionWrapper3);
     
-    $.when(call1, call2, call3)
-    .then(function(response1, response2, response3) {
-        // debugger;
-        var beer = response1[0][0];
-        // var suggestionWrapper = $('<div class="suggestion">')
-        var beerName = $("<p>").text(beer.name);
-        var beerType = $("<p>").text(beer.tagline);
-        var beerImage = $("<img>").attr("src", beer.image_url);
-        var suggestionWrapper = $('<a class="suggestion">');
-        suggestionWrapper.append(beerImage);
+});
 
-
-        // suggestionWrapper.append(beerName, beerType, beerImage);
-        $("#suggestions-div").empty().append(suggestionWrapper).append(suggestionWrapper2);
-        
-    });
 
 // Then use the variables to pull load Favorites from DB
 
